b20f356b784c296f991ef778ec948cad0ea52a7f
Navid <n_doown@yahoo.com>
2019-07-24 00:01:53 +0430

    calculator and drawable

--- app/src/main/java/com/example/bmiweightcalculator/MainActivity.java
+++ app/src/main/java/com/example/bmiweightcalculator/MainActivity.java
@@ -1,27 +1,93 @@
 package com.example.bmiweightcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText edtHeight = findViewById(R.id.edt_Height);
-    EditText edtWeight = findViewById(R.id.edt_Weight);
-    Button btnCalculate= findViewById(R.id.btn_Calculate);
+    EditText edtHeight;
+    EditText edtWeight;
+    Button btnCalculate;
 
+    ImageView bodyIcons;
+    TextView tvBMI;
+    TextView tvRecommendation;
+    TextView tvBodyStatus;
+    ConstraintLayout aboutLayout;
+    ConstraintLayout resultLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        edtHeight = findViewById(R.id.edt_Height);
+        edtWeight = findViewById(R.id.edt_Weight);
+        btnCalculate= findViewById(R.id.btn_Calculate);
+
+        bodyIcons = findViewById(R.id.img_Body_icons);
+        tvBMI = findViewById(R.id.tv_BMI);
+        tvRecommendation = findViewById(R.id.tv_Recommendation);
+        tvBodyStatus = findViewById(R.id.tv_Body_Status);
+        aboutLayout = findViewById(R.id.about);
+        resultLayout = findViewById(R.id.layout_Result);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultLayout.setVisibility(View.VISIBLE);
+                aboutLayout.setVisibility(View.GONE);
+
+                int height = Integer.parseInt(edtHeight.getText().toString());
+                int weight = Integer.parseInt(edtWeight.getText().toString());
+                if (height<1 && weight<1) {
+                    Toast.makeText(MainActivity.this, R.string.warn, Toast.LENGTH_LONG).show();
+                }else {
+
+                    float result = BmiCalculator(weight,height);
+
+                    tvBMI.setText(Float.toString(result));
+
+                    if (result <= 18.5){
+                        tvRecommendation.setText(R.string.under_Weight_Recommendation);
+                        tvBodyStatus.setText(R.string.under_Weight);
+                        tvBodyStatus.setBackgroundResource(R.color.colorBlue);
+                        bodyIcons.setBackgroundResource(R.drawable.ic_under_weight);
+                    }else if (result <= 24.9){
+                        tvRecommendation.setText(R.string.normal_Weight_Recommendation);
+                        tvBodyStatus.setText(R.string.normal_Weight);
+                        tvBodyStatus.setBackgroundResource(R.color.colorGreen);
+                        bodyIcons.setBackgroundResource(R.drawable.ic_normal_weight);
+                    }else if (result <= 34.9){
+                        tvRecommendation.setText(R.string.fat_Recommendation);
+                        tvBodyStatus.setText(R.string.fat);
+                        tvBodyStatus.setBackgroundResource(R.color.colorOrange);
+                        bodyIcons.setBackgroundResource(R.drawable.ic_fat);
+                    }else if (result >= 35){
+                        tvRecommendation.setText(R.string.very_fat_Recommendation);
+                        tvBodyStatus.setText(R.string.very_fat);
+                        tvBodyStatus.setBackgroundResource(R.color.colorRed);
+                        bodyIcons.setBackgroundResource(R.drawable.ic_very_fat);
+                    }
+                }
+            }
+        });
     }
 
-    public double BmiCalculator(int weight , int height){
-        double bmi = 0;
+    public Float BmiCalculator(int weight , int height){
 
-        return bmi;
+        float h = (float) height / 100;
+        float heightSquared = h*h;
+
+        return weight / heightSquared;
     }
 }
